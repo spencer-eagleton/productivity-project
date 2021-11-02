51d3d74276ba0bcc28d420b2256a25aeb402df4b
mira-kine <mirak0419@gmail.com>
2021-11-02 15:49:19 -0700

    added event listeners to homeButton and newTaskButton

--- results/results.js
+++ results/results.js
@@ -1,16 +1,17 @@
-import { getUser, displayUser } from '../utils.js';
+import { getUser, clearStorage } from '../utils.js';
 
 // grab DOM elements needed for results page
 const resultsDisplay = document.getElementById('results-display');
 const divCompleted = document.getElementById('completed-tasks');
 const divIncompleted = document.getElementById('incompleted-tasks');
+const homeButton = document.getElementById('home-button');
+const newTaskButton = document.getElementById('new-task-button');
 // display user on header
-displayUser();
+// displayUser();
 // call getUser() to update results page with 'USER'
 const userObject = getUser();
-
-// loop through 'USER', for each completed: true render ___
-    // else, if completed: false, render _____
+// loop through 'USER', for each completed: true render ___ onto divCompleted
+    // else, if completed: false, render _____ onto divIncompleted
 for (let item of userObject.tasks){
     if (userObject.tasks.completed === true){
         const h3Comp = document.createElement('h3');
@@ -24,3 +25,14 @@ for (let item of userObject.tasks){
     }
     resultsDisplay.append(divCompleted, divIncompleted);
 }
+
+// clears local storage, brings you back to welcome page
+homeButton.addEventListener('click', ()=> {
+    clearStorage();
+});
+
+// go back to user-input page
+newTaskButton.addEventListener('click', ()=> {
+    window.location.replace('../user-input');
+});
+
