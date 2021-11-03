5f04b6323d666ec61b11072d1d9ff0b89d3c6311
mira-kine <mirak0419@gmail.com>
2021-11-03 11:58:27 -0700

    completed checked:true/false

--- timer/timer.js
+++ timer/timer.js
@@ -11,8 +11,6 @@ for (let item of user.tasks){
     const li = document.createElement('li'); 
     const checkbox = document.createElement('input'); 
     checkbox.setAttribute('type', 'checkbox'); 
-        // checkbox.classList.add('checkbox'); 
-        // checkbox.value = item.id; 
     li.textContent = item.id;
     li.append(checkbox); 
     taskContainer.append(li); 
@@ -20,9 +18,8 @@ for (let item of user.tasks){
     if (item.completed === true){
         checkbox.classList.add('completed');
     }
-    checkbox.addEventListener('change', () => {
-        let checkedItems = document.querySelectorAll('input[type=checkbox]:checked');
-        if (checkedItems){
+    checkbox.addEventListener('change', (e) => {
+        if (e.target.checked){
             completeTask(item.id);
         } 
         else {
@@ -33,21 +30,6 @@ for (let item of user.tasks){
 } 
 
 
-
-
 doneButton.addEventListener('click', () => {
     window.location.replace('../results');
 });
-// doneButton.addEventListener('click', () => {
-//     let checkedItems = document.querySelectorAll('input[type=checkbox]:checked'); 
-//     for (let item of checkedItems){
-//         console.log(user.tasks.completed); 
-//     }
-// }); 
-    // find the corresponding item (findById)
-    // change property to true
-    // set back into local storage 
-    // take all of the items in the list
-    // if an item has been selected: 
-    // mark that item as user.tasks.completed === true; 
-    // then, send user to the next page
