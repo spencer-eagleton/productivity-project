6c7e60b221918eedf93e4e0e89733c050e7ab6a1
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-11-02 17:40:00 -0700

    tried adding the checkbox to the list

--- timer/timer.js
+++ timer/timer.js
@@ -5,24 +5,32 @@ const user = getUser();
 displayUser(user);
 
 const taskContainer = document.getElementById('task-list'); 
-
-// const userTasks = user.tasks; 
-// console.log(userTasks[0].id); 
+const doneButton = document.getElementById('done-button'); 
 
 for (let item of user.tasks){
     if (item.completed === false){
-        const li = document.createElement('li'); 
-        const box = document.createElement('input'); 
-        box.setAttribute('type', 'checkbox'); 
+        let li = document.createElement('li'); 
+        let checkbox = document.createElement('input'); 
+        checkbox.setAttribute('type', 'checkbox'); 
+        checkbox.classList.add('checkbox'); 
+        // checkbox.value = item.id; 
         li.textContent = item.id;
-        li.append(box); 
+        li.append(checkbox); 
         taskContainer.append(li); 
-    }} 
+    }
+} 
 
-// loop through each 'task' in LS
-// if task is incomplete, 
-// create item: li
-// li text content = from LS
-// input type = checkbox
 
-// append li onto taskContainer ul
+doneButton.addEventListener('click', () => {
+    let checkedItems = document.querySelectorAll('input[type=checkbox]:checked'); 
+    for (let item of checkedItems){
+        console.log(user.tasks.completed); 
+    }
+}); 
+    // find the corresponding item (findById)
+    // change property to true
+    // set back into local storage 
+    // take all of the items in the list
+    // if an item has been selected: 
+    // mark that item as user.tasks.completed === true; 
+    // then, send user to the next page
